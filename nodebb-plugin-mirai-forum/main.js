edfde7bde798b9e3cd162cd2995a47d30c24c301
Karlatemp <karlatemp@vip.qq.com>
2021-08-13 13:01:50 +0800

    Username register rule

--- nodebb-plugin-mirai-forum/main.js
+++ nodebb-plugin-mirai-forum/main.js
@@ -4,6 +4,7 @@ const topics = require.main.require('./src/topics');
 const posts = require.main.require('./src/posts');
 const privsPosts = require.main.require('./src/privileges/posts');
 const user = require.main.require('./src/user');
+const Meta = require.main.require('./src/meta');
 
 plugin["filter:privileges+groups+list"] = function (privileges, callback) {
     privileges.push('groups:topics:edit-reply');
@@ -19,6 +20,45 @@ async function getPostOwner(pid) {
     let pdata = await posts.getPostFields(pid, ['uid']);
     return pdata.uid;
 }
+
+/**
+ * @param {string | undefined | null} value
+ * @returns {string} 
+ */
+function stringSafe(value) {
+    if (value == undefined) return "";
+    return value.replace('\r\n', '\n').replace('\r', '\n')
+}
+
+plugin["filter:register+check"] = async function (event) {
+
+    /**
+     * @type {string}
+     */
+    let bl = await Meta.configs.get("mirai-forum:username-blacklist");
+    bl = stringSafe(bl);
+
+    for (let rule of bl.split('\n')) {
+        if (rule.trim().length == 0) continue;
+
+        if (RegExp(rule, 'i').test(event.userData.username)) {
+            throw Error("Invalid username");
+        }
+    }
+
+    bl = await Meta.configs.get("mirai-forum:email-blacklist");
+    bl = stringSafe(bl);
+
+    for (let rule of bl.split('\n')) {
+        if (rule.trim().length == 0) continue;
+
+        if (RegExp(rule, 'i').test(event.userData.email)) {
+            throw Error("Invalid email");
+        }
+    }
+
+    return event;
+};
 
 plugin["filter:privileges+posts+edit"] = async function (event) {
     // console.log(event);
@@ -51,5 +91,25 @@ plugin["filter:privileges+posts+edit"] = async function (event) {
     return event;
 };
 
+plugin["static:app+load"] = async function (params) {
+    function renderAdmin(req, res) {
+        console.log("REQ ADMIN");
+        res.render(`admin/plugins/mirai-forum/settings.tpl`, {});
+    }
+
+    params.router.get(`/admin/plugins/mirai-forum-addon`, params.middleware.admin.buildHeader, renderAdmin);
+    params.router.get(`/api/admin/plugins/mirai-forum-addon`, renderAdmin);
+
+    return params;
+};
+
+plugin["filter:admin+header+build"] = async function (adminHeader) {
+    adminHeader.plugins.push({
+        route: `/plugins/mirai-forum-addon`,
+        name: "Mirai Forum Addon",
+    });
+
+    return adminHeader;
+};
 
 module.exports = plugin;
